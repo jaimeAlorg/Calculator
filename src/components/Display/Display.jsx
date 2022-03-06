99116851f16c69e1d8bbef4795074a538749a101
jaime <jaime.alorg1998@gmail.com>
2022-03-06 14:02:47 +0100

    Implemented display + style

---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Display.css';
+
+const Display = () => {
+    return (
+        <div className='display'>
+            <span>(0)</span> 0
+        </div>
+    );
+};
+
+export default Display;
--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Buttons from '../Buttons/Buttons';
+import Display from '../Display/Display';
 import './Panel.css';
 
 const Panel = () => {
     return (
         <div className='panel'>
+            <Display />
             <Buttons />
         </div>
     );
